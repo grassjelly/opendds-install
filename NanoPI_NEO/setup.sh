8fe24d06088153ad00462f2cda681a8d73c6b0f0
jimenojmm@gmail.com <jimenojmm@gmail.com>
2017-01-31 01:12:05 +0800

    added build script

--- NanoPI_NEO/setup.sh
+++ NanoPI_NEO/setup.sh
@@ -5,3 +5,5 @@ apt-get install git
 apt-get install dphys-swapfile
 cp dphys-swapfile /etc
 fs_resize
+echo "alias build_dds=\"MWC=\$(find . -name '*.mwc') && $ACE_ROOT/bin/mwc.pl -type gnuace \$MWC\"" >> ~/.bashrc
+
